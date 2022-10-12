2d989b2aa39b0d186ec82ab024b3890b7df2417e
jibola <pro.ajibolaojo@gmail.com>
2022-10-12 12:01:11 +0100

    some onenft customizations, added harmony shard 0 to wagmi

--- utils/rainbow.ts
+++ utils/rainbow.ts
@@ -1,28 +1,46 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultWallets,
-} from '@rainbow-me/rainbowkit';
-import {
-  chain,
-  configureChains,
-  createClient,
-} from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { Chain, chain, configureChains, createClient } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { HARMONY_RPC_SHARD_0_URL } from "harmony-marketplace-sdk";
+
+const harmonyChain = {
+  id: 1666600000,
+  name: "Harmony Mainnet",
+  network: "harmony",
+  rpcUrls: {
+    default: HARMONY_RPC_SHARD_0_URL,
+  },
+  blockExplorers: {
+    default: {
+      name: "Harmony Explorer",
+      url: "https://explorer.harmony.one/",
+    },
+  },
+  nativeCurrency: {
+    name: "ONE",
+    symbol: "ONE",
+    decimals: 18,
+  },
+  testnet: false,
+} as Chain;
 
 export const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [harmonyChain],
   [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: chain.rpcUrls.default,
+      }),
+    }),
   ]
 );
 const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  chains
+  appName: "ONENFT Marketplace",
+  chains,
 });
 export const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider
-})
+  provider,
+});
