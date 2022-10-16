bf829af494a6202e6684daa82b81a6edc02d7bdd
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-16 19:35:58 +0100

    feat : init homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Hero from "$components/Explore/Hero/Hero";
+import MainLayout from "$layouts/MainLayout/MainLayout";
 import { HarmonyShards, Key } from "harmony-marketplace-sdk";
 import type { NextPage } from "next";
 import Head from "next/head";
@@ -12,7 +13,10 @@ const Home: NextPage = () => {
         <meta name="description" content="An NFT MArketplace and Explorer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <><ConnectButton /></>
+      <MainLayout>
+        <Hero />
+      </MainLayout>
+      {/* <><ConnectButton /></> */}
     </div>
   );
 };
