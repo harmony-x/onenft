76d03f9d9ce17b8c4b0885752fe9b3dc827a6f54
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-18 20:15:24 +0100

    chore : build function to split an array to chunks

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,3 +1,18 @@
 export const applyEllipsis = (text: string, limit: number) => {
   return text && text.length > limit ? `${text.substring(0, limit)}...` : text;
 };
+
+export const splitArrToChunks = <T>(perChunk: number, inputArr: T[]) => {
+  const result = inputArr.reduce<T[][]>((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / perChunk);
+
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = []; // start a new chunk
+    }
+
+    resultArray[chunkIndex].push(item);
+
+    return resultArray;
+  }, []);
+  return result;
+};
