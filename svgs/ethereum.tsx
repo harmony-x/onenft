a0cf20161d62ea045f2be552bc2ba222ee0e4b5b
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-18 20:14:50 +0100

    chore : add svg

---
+++ svgs/ethereum.tsx
@@ -0,0 +1,46 @@
+import { SVGProps } from "react";
+
+export function Ethereum(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      width="12"
+      height="18"
+      viewBox="0 0 12 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_5_1244)">
+        <path
+          d="M5.53695 0L5.41602 0.410072V12.3094L5.53695 12.4298L11.0738 9.16489L5.53695 0Z"
+          fill="#343434"
+        />
+        <path
+          d="M5.53682 0L0 9.16489L5.53682 12.4299V6.6543V0Z"
+          fill="#8C8C8C"
+        />
+        <path
+          d="M5.5369 13.4757L5.46875 13.5586V17.7974L5.5369 17.996L11.077 10.2124L5.5369 13.4757Z"
+          fill="#3C3C3B"
+        />
+        <path
+          d="M5.53682 17.9959V13.4756L0 10.2123L5.53682 17.9959Z"
+          fill="#8C8C8C"
+        />
+        <path
+          d="M5.53711 12.4299L11.0738 9.16495L5.53711 6.65436V12.4299Z"
+          fill="#141414"
+        />
+        <path
+          d="M0 9.16495L5.53673 12.4299V6.65436L0 9.16495Z"
+          fill="#393939"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_5_1244">
+          <rect width="11.0769" height="18" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
