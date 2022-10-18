e5ff8bcd8d1e8d64b23d37e9ce6df933e5c28dba
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-18 21:50:06 +0100

    chore : test disconnect button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "$components/App/Button/Button.styles";
 import Category from "$components/Explore/Category/Category";
 import Hero from "$components/Explore/Hero/Hero";
 import HotCollections from "$components/Explore/HotCollections/HotCollections";
@@ -5,9 +6,11 @@ import MainLayout from "$layouts/MainLayout/MainLayout";
 import { HarmonyShards, Key } from "harmony-marketplace-sdk";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useDisconnect } from "wagmi";
 
 const Home: NextPage = () => {
   const key = new Key(HarmonyShards.SHARD_0);
+  const { disconnect } = useDisconnect();
   return (
     <div>
       <Head>
@@ -19,7 +22,9 @@ const Home: NextPage = () => {
         <Hero />
         <HotCollections />
         <Category />
+        <Button onClick={() => disconnect()}>Disconnect</Button>
       </MainLayout>
+
       {/* <><ConnectButton /></> */}
     </div>
   );
