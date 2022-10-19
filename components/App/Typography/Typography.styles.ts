cb2672bce379ad23ea9645a79b88348888dad0c9
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-19 07:53:34 +0100

    chore : add new typography

--- components/App/Typography/Typography.styles.ts
+++ components/App/Typography/Typography.styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { ITypographyProps } from "./Typography.types";
 
 export const HeadingTwo = styled.h2<ITypographyProps>`
-  font-weight: 500;
+  font-weight: ${({ weight }) => weight ?? "500"};
   font-size: 2.4rem;
   line-height: 3rem;
   letter-spacing: 0.01em;
@@ -20,11 +20,27 @@ export const HeadingTwo = styled.h2<ITypographyProps>`
   }
 `;
 
+export const HeadingThree = styled.h3<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "500"};
+  font-size: 1.8rem;
+  line-height: 2rem;
+  color: rgba(255, 255, 255, 0.87);
+  margin-bottom: ${({ mb }) => mb};
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.sm}) {
+    font-size: 2.6rem;
+    line-height: 2.1rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 3.6rem;
+    line-height: 4rem;
+  }
+`;
+
 export const HeadingFour = styled.h4<ITypographyProps>`
-  font-weight: 500;
+  font-weight: ${({ weight }) => weight ?? "500"};
   font-size: 1.6rem;
   line-height: 2rem;
-  margin-bottom: 2px;
   color: rgba(255, 255, 255, 0.87);
   margin-bottom: ${({ mb }) => mb};
   text-align: ${({ textAlign }) => textAlign};
--- components/App/Typography/Typography.types.ts
+++ components/App/Typography/Typography.types.ts
@@ -3,4 +3,5 @@ import CSSTypes from "csstype";
 export interface ITypographyProps {
   mb?: CSSTypes.Property.MarginBottom;
   textAlign?: CSSTypes.Property.TextAlign;
+  weight?: CSSTypes.Property.FontWeight;
 }
