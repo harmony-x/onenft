8c44449c97f9af7c3699221c8e0c9b7860a6e9e1
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-19 07:53:50 +0100

    feat : add table

--- components/Items/ItemView/ItemView.tsx
+++ components/Items/ItemView/ItemView.tsx
@@ -1,14 +1,18 @@
 import { Button } from "$components/App/Button/Button.styles";
 import CreatorDisplay from "$components/App/CreatorDisplay/CreatorDisplay";
+import { Table } from "$components/App/Table/Table.styles";
 import {
   HeadingFour,
   HeadingTwo,
 } from "$components/App/Typography/Typography.styles";
 import { FlexibleDiv } from "$components/Box/Box.styles";
 import { Harmony } from "$svgs/harmony";
+import { activity } from "$utils/data";
+import { ColumnsType } from "antd/lib/table";
 import Image from "next/image";
 import {
   ItemViewButton,
+  ItemViewPrice,
   ItemViewTab,
   StyledItemView,
   StyledItemViewContent,
@@ -17,6 +21,39 @@ import {
 } from "./ItemView.styles";
 
 const ItemView = () => {
+  const columns: ColumnsType<typeof activity[number]> = [
+    {
+      title: "Event",
+      dataIndex: "event",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      render: (text) => (
+        <ItemViewPrice
+          className="small"
+          alignItems="center"
+          justifyContent="flex-start"
+        >
+          <Harmony className="small" />
+          {text}
+        </ItemViewPrice>
+      ),
+    },
+    {
+      title: "From",
+      dataIndex: "from",
+    },
+    {
+      title: "To",
+      dataIndex: "to",
+    },
+    {
+      title: "Time",
+      dataIndex: "time",
+    },
+  ];
+
   return (
     <StyledItemView
       as="section"
@@ -31,27 +68,29 @@ const ItemView = () => {
         <HeadingTwo mb="6px" as="h3">
           Primate #2679
         </HeadingTwo>
-        <StyledItemViewContentText mb="48px">
+        <StyledItemViewContentText className="owner">
           Owned by <span className="span">DzaLiv</span>
         </StyledItemViewContentText>
         <StyledItemViewContentText mb="15px">
           Current price
         </StyledItemViewContentText>
-        <FlexibleDiv justifyContent="flex-start" margin="0 0 31px 0" gap="12px">
+        <ItemViewPrice justifyContent="flex-start">
           <Harmony />
           <HeadingFour>145</HeadingFour>
-          <StyledItemViewContentText as="sub">$20.56</StyledItemViewContentText>
-        </FlexibleDiv>
+          <StyledItemViewContentText as="p">$20.56</StyledItemViewContentText>
+        </ItemViewPrice>
         <ItemViewButton mb="46px">Buy now</ItemViewButton>
         <ItemViewTab>
-          <ItemViewTab.TabPane
-            key="1"
-            tab="About Creator"
-          ></ItemViewTab.TabPane>
-          <ItemViewTab.TabPane
-            key="2"
-            tab="Item Activity"
-          ></ItemViewTab.TabPane>
+          <ItemViewTab.TabPane key="1" tab="About Creator">
+            Collection of 10,000 Primates facilitating a seamless adoption of
+            the web3 space through community fueled ventures and collaborations.
+          </ItemViewTab.TabPane>
+          <ItemViewTab.TabPane key="2" tab="Item Activity">
+            <Table
+              dataSource={activity}
+              columns={columns as ColumnsType<object>}
+            />
+          </ItemViewTab.TabPane>
         </ItemViewTab>
       </StyledItemViewContent>
     </StyledItemView>
