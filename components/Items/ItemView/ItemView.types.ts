7a3e3540b9ed40ecebf4669bbe2d68d71c5847b7
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-22 20:53:57 +0100

    chore : add new type

--- components/Items/ItemView/ItemView.types.ts
+++ components/Items/ItemView/ItemView.types.ts
@@ -1,5 +1,17 @@
 import CSSTypes from "csstype";
+import React from "react";
 
 export interface IStyledItemViewContentTextProps {
   mb?: CSSTypes.Property.MarginBottom;
+}
+
+export interface IItemViewProps {
+  button: React.ReactNode;
+  creatorName: string;
+  ownerName: string;
+  description: string;
+  itemName: string;
+  itemImage: string;
+  tokenId: string;
+  id: string;
 }
