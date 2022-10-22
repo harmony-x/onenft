29c9d47b1e35bf456d90d63e4b622c0fda5226dd
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-22 20:53:43 +0100

    feat : integrate NFT metadata from covalent API

--- components/Items/ItemView/ItemView.tsx
+++ components/Items/ItemView/ItemView.tsx
@@ -5,13 +5,14 @@ import {
   HeadingFour,
   HeadingTwo,
 } from "$components/App/Typography/Typography.styles";
-import { FlexibleDiv } from "$components/Box/Box.styles";
 import { Harmony } from "$svgs/harmony";
+import { getNFTTransactions, NFTTransaction } from "$utils/api";
 import { activity } from "$utils/data";
+import { applyEllipsis, truncateAddress } from "$utils/functions";
 import { ColumnsType } from "antd/lib/table";
 import Image from "next/image";
+import { FC } from "react";
 import {
-  ItemViewButton,
   ItemViewPrice,
   ItemViewTab,
   StyledItemView,
@@ -19,27 +20,49 @@ import {
   StyledItemViewContentText,
   StyledItemViewImage,
 } from "./ItemView.styles";
+import { IItemViewProps } from "./ItemView.types";
+import { useQuery } from "react-query";
 
-const ItemView = () => {
+const ItemView: FC<IItemViewProps> = ({
+  button,
+  creatorName,
+  description,
+  ownerName,
+  itemName,
+  itemImage,
+  id,
+  tokenId,
+}) => {
+  const { data, isLoading, isSuccess } = useQuery(
+    ["nftTransactionsData", id, tokenId],
+    () => getNFTTransactions(id, tokenId)
+  );
+  const logs: NFTTransaction["log_events"] = [];
+  if (data) {
+    data.items[0].nft_transactions.forEach(({ log_events }) =>
+      log_events.forEach((item) => logs.push(item))
+    );
+  }
+
   const columns: ColumnsType<typeof activity[number]> = [
     {
       title: "Event",
       dataIndex: "event",
     },
-    {
-      title: "Price",
-      dataIndex: "price",
-      render: (text) => (
-        <ItemViewPrice
-          className="small"
-          alignItems="center"
-          justifyContent="flex-start"
-        >
-          <Harmony className="small" />
-          {text}
-        </ItemViewPrice>
-      ),
-    },
+    // {
+    //   title: "Price",
+    //   dataIndex: "price",
+    //   render: (text) => (
+    //     <ItemViewPrice
+    //       className="small"
+    //       alignItems="center"
+    //       justifyContent="flex-start"
+    //     >
+    //       <Harmony className="small" />
+    //       {text}
+    //     </ItemViewPrice>
+    //   ),
+    // },
     {
       title: "From",
       dataIndex: "from",
@@ -61,15 +84,15 @@ const ItemView = () => {
       justifyContent="space-between"
     >
       <StyledItemViewImage>
-        <Image src="/primate.png" objectFit="cover" alt="" layout="fill" />
+        <Image src={itemImage} objectFit="cover" alt="" layout="fill" />
       </StyledItemViewImage>
       <StyledItemViewContent>
-        <CreatorDisplay mb="14px" name="PrimatesXX" img="/primate.png" />
+        <CreatorDisplay mb="14px" name={creatorName} img="/primate.png" />
         <HeadingTwo mb="6px" as="h3">
-          Primate #2679
+          {itemName}
         </HeadingTwo>
         <StyledItemViewContentText className="owner">
-          Owned by <span className="span">DzaLiv</span>
+          Owned by <span className="span">{truncateAddress(ownerName)}</span>
         </StyledItemViewContentText>
         <StyledItemViewContentText mb="15px">
           Current price
@@ -79,17 +102,28 @@ const ItemView = () => {
           <HeadingFour>145</HeadingFour>
           <StyledItemViewContentText as="p">$20.56</StyledItemViewContentText>
         </ItemViewPrice>
-        <ItemViewButton mb="46px">Buy now</ItemViewButton>
+        {button}
         <ItemViewTab>
           <ItemViewTab.TabPane key="1" tab="About Creator">
-            Collection of 10,000 Primates facilitating a seamless adoption of
-            the web3 space through community fueled ventures and collaborations.
+            {description}
           </ItemViewTab.TabPane>
           <ItemViewTab.TabPane key="2" tab="Item Activity">
-            <Table
-              dataSource={activity}
-              columns={columns as ColumnsType<object>}
-            />
+            {data ? (
+              <Table
+                dataSource={logs
+                  .filter(
+                    ({ decoded }) => decoded?.name.toLowerCase() === "transfer"
+                  )
+                  .map(({ block_signed_at, decoded }) => ({
+                    from: truncateAddress(decoded.params[0].value ?? ""),
+                    to: truncateAddress(decoded.params[1].value ?? ""),
+                    time: block_signed_at?.substring(0, 10),
+                    event: decoded.name,
+                  }))}
+                columns={columns as ColumnsType<object>}
+                loading={isLoading}
+              />
+            ) : null}
           </ItemViewTab.TabPane>
         </ItemViewTab>
       </StyledItemViewContent>
