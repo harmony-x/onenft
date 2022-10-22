44d57dab9df9ab5b5baece7468f3b5cc309a3ed6
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-22 21:00:04 +0100

    feat : import local fonts

--- styles/FontStyles.ts
+++ styles/FontStyles.ts
@@ -3,29 +3,33 @@ import { createGlobalStyle } from "styled-components";
 export const FontStyles = createGlobalStyle`
   @font-face {
     font-family: "Stapel";
-    src: url("/fonts/Stapel_Bold.otf");
+    src: url("/fonts/Stapel_Bold.woff2") format("woff2");
     font-weight: 700;
     font-style: normal;
+    font-display: swap
   }
 
   @font-face {
     font-family: "Stapel";
-    src: url("/fonts/Stapel_Medium.otf");
+    src: url("/fonts/Stapel_Medium.woff2") format("woff2");
     font-weight: 500;
     font-style: normal;
+    font-display: swap
   }
 
   @font-face {
     font-family: "Stapel";
-    src: url("/fonts/Stapel_Regular.otf");
+    src: url("/fonts/Stapel_Regular.woff2") format("woff2");
     font-weight: 400;
     font-style: normal;
+    font-display: swap
   }
 
   @font-face {
     font-family: "Stapel";
-    src: url("/fonts/Stapel_Light.otf");
+    src: url("/fonts/Stapel_Light.woff2") format("woff2");
     font-weight: 300;
     font-style: normal;
+    font-display: swap
   }
 `;
