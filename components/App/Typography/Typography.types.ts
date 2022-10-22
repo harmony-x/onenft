3d92eb0fca910994f7b56817b0e6506a87f8c957
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-22 20:51:01 +0100

    chore : add color props

--- components/App/Typography/Typography.types.ts
+++ components/App/Typography/Typography.types.ts
@@ -3,5 +3,6 @@ import CSSTypes from "csstype";
 export interface ITypographyProps {
   mb?: CSSTypes.Property.MarginBottom;
   textAlign?: CSSTypes.Property.TextAlign;
+  color?: CSSTypes.Property.Color;
   weight?: CSSTypes.Property.FontWeight;
 }
