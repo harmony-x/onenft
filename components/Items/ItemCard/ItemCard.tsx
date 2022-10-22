0dd92ff6ef8b7afed164707c6ceecd3dc9095d46
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-22 20:52:44 +0100

    feat : integrate NFT metadata from covalent API

--- components/Items/ItemCard/ItemCard.tsx
+++ components/Items/ItemCard/ItemCard.tsx
@@ -1,45 +1,82 @@
 import { FlexibleDiv } from "$components/Box/Box.styles";
 import { Harmony } from "$svgs/harmony";
-import Image from "next/image";
 import { FC } from "react";
 import {
   StyledItemCard,
   StyledItemCardContent,
+  StyledItemCardContentSkeleton,
   StyledItemCardCreator,
   StyledItemCardDetails,
   StyledItemCardImage,
   StyledItemCardName,
   StyledItemCardPrice,
+  StyledItemCardImageSkeleton,
 } from "./ItemCard.styles";
 import { IItemCardProps } from "./ItemCard.types";
+import { useQuery } from "react-query";
+import { getSingleNFTMetaData } from "$utils/api";
+import { Image, Skeleton } from "antd";
 
-const ItemCard: FC<IItemCardProps> = ({ creator, image, name, price }) => {
+const ItemCard: FC<IItemCardProps> = ({ id, tokenId }) => {
+  const { data, isLoading, isSuccess } = useQuery(
+    ["nftMetaData", id, tokenId],
+    () => getSingleNFTMetaData(id, tokenId)
+  );
+
   return (
     <StyledItemCard>
-      <StyledItemCardImage>
-        <Image objectFit="cover" src={image} alt="" layout="fill" />
-      </StyledItemCardImage>
-      <StyledItemCardContent>
-        <StyledItemCardCreator>
-          <span>By</span> {creator}
-        </StyledItemCardCreator>
-        <StyledItemCardDetails>
-          <StyledItemCardName>{name}</StyledItemCardName>
-          <StyledItemCardPrice>
-            <span className="span">Price:</span> <br />
-            <FlexibleDiv
-              justifyContent="flex-start"
-              flexWrap="nowrap"
-              alignItems="center"
-            >
-              <span className="svg">
-                <Harmony />
-              </span>
-              {price}
-            </FlexibleDiv>
-          </StyledItemCardPrice>
-        </StyledItemCardDetails>
-      </StyledItemCardContent>
+      {isLoading || !data ? (
+        <StyledItemCardImageSkeleton active shape="square" />
+      ) : (
+        <StyledItemCardImage>
+          <Image
+            src={data.items[0].nft_data[0]?.external_data.image ?? "" ?? ""}
+            alt=""
+            // layout="fill"
+            width="100%"
+            height="100%"
+            placeholder={
+              <Image
+                alt=""
+                preview={false}
+                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkE+WrBwABHACybY9s4AAAAABJRU5ErkJggg=="
+                width="100%"
+                height="100%"
+              />
+            }
+            preview={false}
+            // placeholder="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkE+WrBwABHACybY9s4AAAAABJRU5ErkJggg=="
+            // blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkE+WrBwABHACybY9s4AAAAABJRU5ErkJggg=="
+          />
+        </StyledItemCardImage>
+      )}
+      {isLoading || !data ? (
+        <StyledItemCardContentSkeleton paragraph={{ rows: 1 }} />
+      ) : (
+        <StyledItemCardContent>
+          <StyledItemCardCreator>
+            <span>By</span> {data.items[0].contract_name ?? ""}
+          </StyledItemCardCreator>
+          <StyledItemCardDetails>
+            <StyledItemCardName>
+              {data.items[0].nft_data[0]?.external_data.name ?? ""}
+            </StyledItemCardName>
+            <StyledItemCardPrice>
+              <span className="span">Price:</span> <br />
+              <FlexibleDiv
+                justifyContent="flex-start"
+                flexWrap="nowrap"
+                alignItems="center"
+              >
+                <span className="svg">
+                  <Harmony />
+                </span>
+                50
+              </FlexibleDiv>
+            </StyledItemCardPrice>
+          </StyledItemCardDetails>
+        </StyledItemCardContent>
+      )}
     </StyledItemCard>
   );
 };
