83c97c115ab2b43a0884cef1c34663f0e9524fef
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-22 20:50:48 +0100

    feat : add new typography styles

--- components/App/Typography/Typography.styles.ts
+++ components/App/Typography/Typography.styles.ts
@@ -37,6 +37,23 @@ export const HeadingThree = styled.h3<ITypographyProps>`
   }
 `;
 
+export const ParagraphOne = styled.h3<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "500"};
+  font-size: 1.6rem;
+  line-height: 2rem;
+  color: ${({ color }) => color ?? "rgba(255, 255, 255, 0.67)"};
+  margin-bottom: ${({ mb }) => mb};
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.sm}) {
+    font-size: 2.4rem;
+    line-height: 2.1rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 3.2rem;
+    line-height: 4rem;
+  }
+`;
+
 export const HeadingFour = styled.h4<ITypographyProps>`
   font-weight: ${({ weight }) => weight ?? "500"};
   font-size: 1.6rem;
@@ -51,5 +68,56 @@ export const HeadingFour = styled.h4<ITypographyProps>`
   @media screen and (${BREAKPOINTS.lg}) {
     font-size: 2.8rem;
     line-height: 4.8rem;
+  }
+`;
+
+export const HeadingFive = styled.h5<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "500"};
+  font-size: 1.6rem;
+  line-height: 2rem;
+  color: ${({ color }) => color ?? "rgba(255, 255, 255, 0.87)"};
+  margin-bottom: ${({ mb }) => mb};
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.sm}) {
+    font-size: 2rem;
+    line-height: 2.2rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 2.4rem;
+    line-height: 3rem;
+  }
+`;
+
+export const HeadingSix = styled.h6<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "500"};
+  font-size: 1.4rem;
+  line-height: 1.6rem;
+  color: ${({ color }) => color ?? "rgba(255, 255, 255, 0.87)"};
+  margin-bottom: ${({ mb }) => mb};
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.sm}) {
+    font-size: 1.6rem;
+    line-height: 2rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 2rem;
+    line-height: 2.8rem;
+  }
+`;
+
+export const ParagraphTwo = styled.p<ITypographyProps>`
+  font-weight: ${({ weight }) => weight ?? "500"};
+  font-size: 1.2rem;
+  line-height: 1.4rem;
+  color: ${({ color }) => color ?? "rgba(255, 255, 255, 0.87)"};
+  margin-bottom: ${({ mb }) => mb};
+  text-align: ${({ textAlign }) => textAlign};
+  @media screen and (${BREAKPOINTS.sm}) {
+    font-size: 1.4rem;
+    line-height: 2rem;
+  }
+  @media screen and (${BREAKPOINTS.lg}) {
+    font-size: 1.8rem;
+    line-height: 2.5rem;
   }
 `;
