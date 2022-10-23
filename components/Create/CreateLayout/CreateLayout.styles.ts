4464c291bc610332ac6fb6d9a58cf6f567365865
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-10-24 00:05:17 +0100

    style : style components

--- components/Create/CreateLayout/CreateLayout.styles.ts
+++ components/Create/CreateLayout/CreateLayout.styles.ts
@@ -1,4 +1,5 @@
 import { Button } from "$components/App/Button/Button.styles";
+import { FlexibleDiv } from "$components/Box/Box.styles";
 import { BREAKPOINTS } from "$constants/breakpoints";
 import styled from "styled-components";
 
@@ -15,4 +16,19 @@ export const CreateLayout = styled.section`
 export const CreateItemButton = styled(Button)`
   max-width: 522px;
   width: 100%;
+`;
+
+export const MyCollectionsButton = styled(Button)`
+  max-width: 371px;
+  width: 100%;
+  margin-bottom: 50px;
+  @media screen and (${BREAKPOINTS.lg}) {
+    margin-bottom: 80px;
+  }
+`;
+
+export const EmptyCollections = styled(FlexibleDiv)`
+  border: 2px solid #3d405c;
+  border-radius: 10px;
+  height: 349px;
 `;
